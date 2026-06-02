79c6d544f672c7ade031523ccd8ecf532e8b63d0
kiyo99 <kiofavour@gmail.com>
2026-06-02 15:59:45 -0400

    Initial Commit

---
+++ URL Organizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  URL Organizer
+//
+//  Created by Godsfavour Ngo Kio on 2026-06-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ URL Organizer/URL_OrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  URL_OrganizerApp.swift
+//  URL Organizer
+//
+//  Created by Godsfavour Ngo Kio on 2026-06-02.
+//
+
+import SwiftUI
+
+@main
+struct URL_OrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
