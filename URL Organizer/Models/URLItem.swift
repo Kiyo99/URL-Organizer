3dd6700ac1f3637e0d5e8d8e69a650a1244b9e76
kiyo99 <kiofavour@gmail.com>
2026-06-02 16:24:09 -0400

    Initial folder/architecture setup

---
+++ URL Organizer/Models/URLItem.swift
@@ -0,0 +1,12 @@
+//
+//  URLItem.swift
+//  URL Organizer
+//
+//  Created by Godsfavour Ngo Kio on 2026-06-02.
+//
+import Foundation
+
+struct URLItem: Identifiable {
+    let id: UUID = UUID()
+    var urlString: String
+}
---
+++ URL Organizer/ViewModels/URLViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  URLViewModel.swift
+//  URL Organizer
+//
+//  Created by Godsfavour Ngo Kio on 2026-06-02.
+//
+
+import Foundation
+
+@Observable
+@MainActor
+class URLViewModel {
+    var urlList: [URLItem] = []
+    
+    // MARK: - Functions to be called from the View
+    // TODO: - edit, remove, removeAll, removeDuplicates, addURL, validateURL
+}
