8876446c109b42e3083fadac7a65beb18aafe247
kiyo99 <kiofavour@gmail.com>
2026-06-02 17:13:05 -0400

    Feat: Created initial ViewModel functions and tests with all tests passing

--- URL Organizer/ViewModels/URLViewModel.swift
+++ URL Organizer/ViewModels/URLViewModel.swift
@@ -11,7 +11,54 @@ import Foundation
 @MainActor
 class URLViewModel {
     var urlList: [URLItem] = []
+    var selectedURLItem: URLItem? = nil
+    var currentURL: String = ""
+    var validationErrorMessage: String = ""
     
     // MARK: - Functions to be called from the View
     // TODO: - edit, remove, removeAll, removeDuplicates, addURL, validateURL
+    
+    func validateURL() -> Bool {
+        validationErrorMessage = ""
+        guard let safeUrl = URL(string: currentURL),
+              safeUrl.scheme?.lowercased() == "https" || safeUrl.scheme?.lowercased() == "http",
+              safeUrl.host != nil else {
+            validationErrorMessage = "Please enter a valid URL."
+            return false
+        }
+        return true
+    }
+    
+    func addURL() {
+        guard !currentURL.isEmpty, validateURL() else { return }
+        urlList.append(URLItem(urlString: currentURL))
+        currentURL = ""
+    }
+    
+    func removeAllURLs() {
+        urlList.removeAll()
+    }
+    
+    func removeUrlItem(_ item: URLItem){
+        urlList.removeAll { $0.id == item.id }
+    }
+    
+    func removeDuplicates() {
+        
+    }
+    
+    func editURL(_ item: URLItem){
+        selectedURLItem = item
+        currentURL = item.urlString
+    }
+    
+    func saveEdits(){
+        guard let selectedURLItem, !currentURL.isEmpty, validateURL() else { return }
+        // find the index of the selectedItem in the urlList
+        if let index = urlList.firstIndex(where: {$0.id == selectedURLItem.id}){
+            urlList[index].urlString = currentURL
+        }
+        self.selectedURLItem = nil
+        currentURL = ""
+    }
 }
--- URL OrganizerTests/URL_OrganizerTests.swift
+++ URL OrganizerTests/URL_OrganizerTests.swift
@@ -8,31 +8,186 @@
 import XCTest
 @testable import URL_Organizer
 
+@MainActor
 final class URL_OrganizerTests: XCTestCase {
+    
+    // MARK: - Properties
+    var urlViewModel: URLViewModel!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        urlViewModel = URLViewModel()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        urlViewModel = nil
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+    
+    // MARK: - validateURL
+    func test_validateURL_withValidHTTPSURL_returnsTrue(){
+        // Arrange
+        urlViewModel.currentURL = "https://mozilla.org"
+        XCTAssertTrue(urlViewModel.validateURL())
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_validateURL_withValidHTTPURL_returnsTrue() {
+        urlViewModel.currentURL = "http://mozilla.org"
+        XCTAssertTrue(urlViewModel.validateURL())
+    }
+    
+    func test_validateURL_withEmptyString_returnsFalse() {
+        urlViewModel.currentURL = ""
+        XCTAssertFalse(urlViewModel.validateURL())
+    }
+    
+    func test_validateURL_withNoScheme_returnsFalse() {
+        urlViewModel.currentURL = "godsfavourkio.com"
+        XCTAssertFalse(urlViewModel.validateURL())
+    }
+    
+    func test_validateURL_withSingleLetter_returnsFalse() {
+        urlViewModel.currentURL = "g"
+        XCTAssertFalse(urlViewModel.validateURL())
+    }
+    
+    func test_validateURL_withValidAllCapsURL_returnsTrue() {
+        urlViewModel.currentURL = "HTTPS://MOZILLA.ORG"
+        XCTAssertTrue(urlViewModel.validateURL())
+    }
+    
+    func test_validateURL_withInvalidScheme_returnsFalse() {
+        urlViewModel.currentURL = "kio://mozilla.org"
+        XCTAssertFalse(urlViewModel.validateURL())
+    }
+    
+    func test_validateURL_withInvalidURL_setsErrorMessage() {
+        urlViewModel.currentURL = "not a url"
+        let _ = urlViewModel.validateURL()
+        XCTAssertFalse(urlViewModel.validationErrorMessage.isEmpty)
+    }
+    
+    func test_validateURL_withValidURL_clearsErrorMessage() {
+        urlViewModel.validationErrorMessage = "some previous error"
+        urlViewModel.currentURL = "https://mozilla.org"
+        let _ = urlViewModel.validateURL()
+        XCTAssertTrue(urlViewModel.validationErrorMessage.isEmpty)
+    }
+    
+    // MARK: - addURL
+    func test_addURL_withValidURL_appendsToList() {
+        urlViewModel.currentURL = "https://mozilla.org"
+        urlViewModel.addURL()
+        XCTAssertEqual(urlViewModel.urlList.count, 1)
+    }
+    
+    func test_addURL_withValidURL_clearsCurrentURL() {
+        urlViewModel.currentURL = "https://mozilla.org"
+        urlViewModel.addURL()
+        XCTAssertTrue(urlViewModel.currentURL.isEmpty)
+    }
+    
+    func test_addURL_withEmptyURL_doesNotAppendToList() {
+        urlViewModel.currentURL = ""
+        urlViewModel.addURL()
+        XCTAssertTrue(urlViewModel.urlList.isEmpty)
+    }
+    
+    func test_addURL_withInvalidURL_doesNotAppendToList() {
+        urlViewModel.currentURL = "not-a-url"
+        urlViewModel.addURL()
+        XCTAssertTrue(urlViewModel.urlList.isEmpty)
+    }
+    
+    func test_addURL_multipleTimes_appendsAllToList() {
+        urlViewModel.currentURL = "https://mozilla.org"
+        urlViewModel.addURL()
+        urlViewModel.currentURL = "https://mozilla.org"
+        urlViewModel.addURL()
+        XCTAssertEqual(urlViewModel.urlList.count, 2)
+    }
+    
+    // MARK: - removeUrlItem
+    func test_removeUrlItem_removesCorrectItem() {
+        urlViewModel.currentURL = "https://mozilla.org"
+        urlViewModel.addURL()
+        let item = urlViewModel.urlList[0]
+        urlViewModel.removeUrlItem(item)
+        XCTAssertTrue(urlViewModel.urlList.isEmpty)
+    }
+    
+    func test_removeUrlItem_onlyRemovesTargetItem() {
+        urlViewModel.currentURL = "https://mozilla.org"
+        urlViewModel.addURL()
+        urlViewModel.currentURL = "https://apple.com"
+        urlViewModel.addURL()
+        let itemToRemove = urlViewModel.urlList[0]
+        urlViewModel.removeUrlItem(itemToRemove)
+        XCTAssertEqual(urlViewModel.urlList.count, 1)
+        XCTAssertEqual(urlViewModel.urlList[0].urlString, "https://apple.com")
+    }
+    
+    // MARK: - removeAllURLs
+    func test_removeAllURLs_clearsEntireList() {
+        urlViewModel.currentURL = "https://mozilla.org"
+        urlViewModel.addURL()
+        urlViewModel.currentURL = "https://apple.com"
+        urlViewModel.addURL()
+        urlViewModel.removeAllURLs()
+        XCTAssertTrue(urlViewModel.urlList.isEmpty)
+    }
+    
+    func test_removeAllURLs_onEmptyList_doesNotCrash() {
+        urlViewModel.removeAllURLs()
+        XCTAssertTrue(urlViewModel.urlList.isEmpty)
+    }
+    
+    // MARK: - editURL
+    func test_editURL_setsSelectedURLItem() {
+        urlViewModel.currentURL = "https://mozilla.org"
+        urlViewModel.addURL()
+        let item = urlViewModel.urlList[0]
+        urlViewModel.editURL(item)
+        XCTAssertEqual(urlViewModel.selectedURLItem?.id, item.id)
+    }
+    
+    func test_editURL_setsCurrentURLToItemURLString() {
+        urlViewModel.currentURL = "https://mozilla.org"
+        urlViewModel.addURL()
+        let item = urlViewModel.urlList[0]
+        urlViewModel.editURL(item)
+        XCTAssertEqual(urlViewModel.currentURL, "https://mozilla.org")
+    }
+    
+    // MARK: - saveEdits
+    func test_saveEdits_updatesItemInList() {
+        urlViewModel.currentURL = "https://mozilla.org"
+        urlViewModel.addURL()
+        let item = urlViewModel.urlList[0]
+        urlViewModel.editURL(item)
+        urlViewModel.currentURL = "https://apple.com"
+        urlViewModel.saveEdits()
+        XCTAssertEqual(urlViewModel.urlList[0].urlString, "https://apple.com")
+    }
+    
+    func test_saveEdits_clearsSelectedURLItem() {
+        urlViewModel.currentURL = "https://mozilla.org"
+        urlViewModel.addURL()
+        let item = urlViewModel.urlList[0]
+        urlViewModel.editURL(item)
+        urlViewModel.currentURL = "https://apple.com"
+        urlViewModel.saveEdits()
+        XCTAssertNil(urlViewModel.selectedURLItem)
+    }
+    
+    func test_saveEdits_clearsCurrentURL() {
+        urlViewModel.currentURL = "https://mozilla.org"
+        urlViewModel.addURL()
+        let item = urlViewModel.urlList[0]
+        urlViewModel.editURL(item)
+        urlViewModel.currentURL = "https://apple.com"
+        urlViewModel.saveEdits()
+        XCTAssertTrue(urlViewModel.currentURL.isEmpty)
     }
 
 }
